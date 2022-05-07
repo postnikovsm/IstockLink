a050e6e7942f8eb451df6a5eb1b2596351b25d64
Olga <olga.dubrovina.olga@gmail.com>
2022-05-07 20:48:21 +0300

    ver 1.3

--- src/test/java/tests/clicks/MenuButton.java
+++ src/test/java/tests/clicks/MenuButton.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -14,7 +15,7 @@ public class MenuButton {
         Configuration.browserSize = "1400x800";
     }
     @Test
-    void clickAllButton(){
+    void clickAllButton() {
         open("https://messenger.test.istock.link/login");
         $("input[name='email']").setValue("istock.link.test@rambler.ru");
         $("input[name='password']").setValue("123123123");
@@ -39,62 +40,84 @@ public class MenuButton {
 //свернуть меню
         $("div[class='snm-header']").click();
 //переход по свернутым разделам
-        $(".snm-short-section",4).click();
+        $(".snm-short-section", 4).click();
         $(".breadcrumbs__item").shouldHave(text("Задачи"));
 
-        $(".snm-short-section",3).click();
+        $(".snm-short-section", 3).click();
         $(".breadcrumbs__item").shouldHave(text("Заявки на закупку"));
 
-        $(".snm-short-section",2).click();
+        $(".snm-short-section", 2).click();
         $(".breadcrumbs__item").shouldHave(text("Данные о компании"));
 
-        $(".snm-short-section",1).click();
+        $(".snm-short-section", 1).click();
         $(".breadcrumbs__item").shouldHave(text("Мой профиль"));
 
-        $(".snm-short-section",0).click();
+        $(".snm-short-section", 0).click();
         $(".layout-messenger__header").shouldHave(text("Мессенджер"));
 
         //переход по наведению
-        $(".snm-short-section",4).hover();
-        $(".snm-short-tooltip",4).click();
+        $(".snm-short-section", 4).hover();
+        $(".snm-short-tooltip", 4).click();
         $(".breadcrumbs__item").shouldHave(text("Задачи"));
 
-        $(".snm-short-section",3).hover();
-        $(".snm-short-tooltip",3).click();
+        $(".snm-short-section", 3).hover();
+        $(".snm-short-tooltip", 3).click();
         $(".breadcrumbs__item").shouldHave(text("Заявки на закупку"));
 
-        $(".snm-short-section",2).hover();
-        $(".snm-short-tooltip",2).$(byText("Моя организация")).click();
+        $(".snm-short-section", 2).hover();
+        $(".snm-short-tooltip", 2).$(byText("Моя организация")).click();
         $(".breadcrumbs__item").shouldHave(text("Данные о компании"));
-        $(".snm-short-section",2).hover();
-        $(".snm-short-tooltip",2).$(byText("Партнеры")).click();
+        $(".snm-short-section", 2).hover();
+        $(".snm-short-tooltip", 2).$(byText("Партнеры")).click();
         $(".breadcrumbs__item").shouldHave(text("Мои партнеры"));
-        $(".snm-short-section",2).hover();
-        $(".snm-short-tooltip",2).$(byText("Сотрудники")).click();
+        $(".snm-short-section", 2).hover();
+        $(".snm-short-tooltip", 2).$(byText("Сотрудники")).click();
         $(".breadcrumbs__item").shouldHave(text("Сотрудники"));
 
-        $(".snm-short-section",1).hover();
-        $(".snm-short-tooltip",1).$(byText("Мой профиль")).click();
+        $(".snm-short-section", 1).hover();
+        $(".snm-short-tooltip", 1).$(byText("Мой профиль")).click();
         $(".breadcrumbs__item").shouldHave(text("Мой профиль"));
-        $(".snm-short-section",1).hover();
-        $(".snm-short-tooltip",1).$(byText("Хранилище файлов")).click();
+        $(".snm-short-section", 1).hover();
+        $(".snm-short-tooltip", 1).$(byText("Хранилище файлов")).click();
         $(".breadcrumbs__item").shouldHave(text("Хранилище файлов"));
 
-        $(".snm-short-section",0).hover();
-        $(".snm-short-tooltip",0).$(byText("Мессенджер")).click();
+        $(".snm-short-section", 0).hover();
+        $(".snm-short-tooltip", 0).$(byText("Мессенджер")).click();
         $(".layout-messenger__header").shouldHave(text("Мессенджер"));
         // резвернуть меню
         $(".snm-header__arrow").click();
         sleep(2000
         );
+    }
 
+        @Test
+        void clickGB(){
+            open("https://messenger.test.istock.link/login");
+            $("input[name='email']").setValue("istock.link.test@rambler.ru");
+            $("input[name='password']").setValue("123123123");
+            $("button[name='loginUserButton']").click();
+            $(".layout-messenger").shouldHave(text("Мессенджер"));
 
+            $(".snm__sections").$(byText("Перейти в Goodwix")).click();
+            $(".logo").shouldHave(visible);
+            switchTo().window(0);
+            $("div[class='snm-header']").click();
+            $(".snm-short-section",5).click();
+            $(".logo").shouldHave(visible);
+            sleep(2000);
 
+        }
 
-
-
+    @Test
+    void clickTopMenu(){
+        open("https://messenger.test.istock.link/login");
+        $("input[name='email']").setValue("istock.link.test@rambler.ru");
+        $("input[name='password']").setValue("123123123");
+        $("button[name='loginUserButton']").click();
+        $(".layout-messenger").shouldHave(text("Мессенджер"));
 
 
 
     }
+
 }
